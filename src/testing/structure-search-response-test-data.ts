96cd567a5a179a63266aa35e4bf356ffb81112df
Jorge Neyra <jorge.neyra@nih.gov>
2018-12-11 10:04:49 -0500

    added overview section to substance details

---
+++ src/testing/structure-search-response-test-data.ts
@@ -0,0 +1,16 @@
+import { StructureSearchResponse } from '../app/substance/structure-search-response.model';
+
+/* tslint:disable:max-line-length quotemark */
+export const StructureSearchResponseTestData: StructureSearchResponse = {
+    "start": 1544373508714,
+    "id": "bhdtblgdkn",
+    "key": "8a2bfee3d8192b5ed9a476ee4511aea6d8bd5c32",
+    "count": 2,
+    "status": "Running",
+    "determined": false,
+    "finished": true,
+    "generatingUrl": "https://ginas.ncats.nih.gov/ginas/app/api/v1/substances/structureSearch?view=full&q=2ab7e065-9f58-404b-85f0-22ca4a82d1b4&type=substructure",
+    "results": "https://ginas.ncats.nih.gov/ginas/app/api/v1/status(8a2bfee3d8192b5ed9a476ee4511aea6d8bd5c32)/results",
+    "url": "https://ginas.ncats.nih.gov/ginas/app/api/v1/status(8a2bfee3d8192b5ed9a476ee4511aea6d8bd5c32)"
+};
+
