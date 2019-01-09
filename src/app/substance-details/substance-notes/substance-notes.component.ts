70cb9864e77380e64ffb7d6011bd0474f65c5234
Neyra <neyraj2@nih.gov>
2019-01-09 11:25:25 -0500

    added notes section

--- src/app/substance-details/substance-notes/substance-notes.component.ts
+++ src/app/substance-details/substance-notes/substance-notes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SubstanceCardBase } from '../substance-card-base';
+import { SubstanceNote } from '../../substance/substance.model';
 
 @Component({
   selector: 'app-substance-notes',
@@ -7,12 +8,17 @@ import { SubstanceCardBase } from '../substance-card-base';
   styleUrls: ['./substance-notes.component.scss']
 })
 export class SubstanceNotesComponent extends SubstanceCardBase implements OnInit {
+  notes: Array<SubstanceNote> = [];
+  displayedColumns: string[] = ['note'];
 
   constructor() {
     super();
   }
 
   ngOnInit() {
+    if (this.substance != null && this.substance.notes != null && this.substance.notes.length) {
+      this.notes = this.substance.notes;
+    }
   }
 
 }
--- src/app/substance-details/substance-notes/substance-notes.module.ts
+++ src/app/substance-details/substance-notes/substance-notes.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SubstanceNotesComponent } from './substance-notes.component';
 import { DynamicComponentLoaderModule } from '../../dynamic-component-loader/dynamic-component-loader.module';
+import { MatTableModule } from '@angular/material/table';
+import { CdkTableModule } from '@angular/cdk/table';
 
 @NgModule({
   imports: [
     CommonModule,
-    DynamicComponentLoaderModule.forChild(SubstanceNotesComponent)
+    DynamicComponentLoaderModule.forChild(SubstanceNotesComponent),
+    MatTableModule,
+    CdkTableModule
   ],
   declarations: [SubstanceNotesComponent]
 })
