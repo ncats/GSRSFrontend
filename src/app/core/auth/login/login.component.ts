96d47fa085a647adb778141703b889dbe749f1a9
Jorge Neyra <jorge.neyra@nih.gov>
2019-04-30 12:24:09 -0400

    finished authentication feature

--- src/app/core/auth/login/login.component.ts
+++ src/app/core/auth/login/login.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { NavigationExtras, Router, ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { LoadingService } from '../../loading/loading.service';
+import { MainNotificationService } from '../../main-notification/main-notification.service';
+import { AppNotification, NotificationType } from '../../main-notification/notification.model';
 
 @Component({
   selector: 'app-login',
@@ -18,31 +21,50 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private loadingService: LoadingService,
+    private mainNotificationService: MainNotificationService
   ) { }
 
   ngOnInit() {
+    this.loadingService.setLoading(true);
     this.authService.getAuth().subscribe(auth => {
+      this.loadingService.setLoading(false);
       if (auth) {
         this.router.navigate(['/browse-substance']);
       } else {
         this.isLoaded = true;
         this.isLoading = false;
       }
+    }, error => {
+      this.loadingService.setLoading(false);
+      this.isLoaded = true;
+      this.isLoading = false;
     });
   }
 
   login() {
     if (this.loginForm.valid) {
+      this.loadingService.setLoading(true);
       this.isLoading = true;
       const username = this.loginForm.controls.username.value;
       const password = this.loginForm.controls.password.value;
       this.authService.login(username, password).subscribe(auth => {
+        this.loadingService.setLoading(false);
         if (auth) {
           this.router.navigate(['/browse-substance']);
         } else {
           this.isLoading = false;
         }
+      }, error => {
+        const notification: AppNotification = {
+          message: 'There was an error logging you in. Please check your credentials and try again.',
+          type: NotificationType.error
+        };
+        this.mainNotificationService.setNotification(notification);
+        this.loadingService.setLoading(false);
+        this.isLoaded = true;
+        this.isLoading = false;
       });
     }
   }
