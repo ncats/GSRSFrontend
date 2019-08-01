811ca69c0d2a316c9831fd8f66126e2b49a2510a
Neyra <neyraj2@nih.gov>
2019-08-01 15:29:57 -0400

    udpated paramter form and dialog

--- src/app/core/substance-form/property-parameter-form/property-parameter-form.component.ts
+++ src/app/core/substance-form/property-parameter-form/property-parameter-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { SubstanceParameter } from '@gsrs-core/substance/substance.model';
+import { ControlledVocabularyService } from '../../controlled-vocabulary/controlled-vocabulary.service';
+import { VocabularyTerm } from '../../controlled-vocabulary/vocabulary.model';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-property-parameter-form',
@@ -8,18 +11,42 @@ import { SubstanceParameter } from '@gsrs-core/substance/substance.model';
 })
 export class PropertyParameterFormComponent implements OnInit {
   private privateParameter: SubstanceParameter;
+  nameControl = new FormControl('', [Validators.required]);
+  propertyTypeList: Array<VocabularyTerm> = [];
+  parameterTypeControl = new FormControl('');
 
-  constructor() { }
+  constructor(
+    private cvService: ControlledVocabularyService
+  ) { }
 
   ngOnInit() {
+    this.getVocabularies();
   }
 
   @Input()
   set parameter(parameter: SubstanceParameter) {
     this.privateParameter = parameter;
+    this.nameControl.setValue(this.privateParameter.name);
+    this.nameControl.valueChanges.subscribe(value => {
+      this.privateParameter.name = value;
+    });
+    this.parameterTypeControl.setValue(this.privateParameter.type);
+    this.parameterTypeControl.valueChanges.subscribe(value => {
+      this.privateParameter.type = value;
+    });
   }
 
   get parameter(): SubstanceParameter {
     return this.privateParameter;
+  }
+
+  getVocabularies(): void {
+    this.cvService.getDomainVocabulary('PROPERTY_TYPE').subscribe(response => {
+      this.propertyTypeList = response['PROPERTY_TYPE'].list;
+    });
+  }
+
+  get isValid(): boolean {
+    return;
   }
 }
