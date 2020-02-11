fc15d4fb777766c75e9ef4a308a533aac9671998
nanders0 <nikolaus.g.anderson@gmail.com>
2020-02-11 17:08:48 -0500

    fixing import

--- src/app/core/substance-details/substance-polymer-structure/substance-polymer-structure.module.ts
+++ src/app/core/substance-details/substance-polymer-structure/substance-polymer-structure.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DynamicComponentLoaderModule } from '../../dynamic-component-loader/dynamic-component-loader.module';
 import { SubstancePolymerStructureComponent } from './substance-polymer-structure.component';
-import {RouterLinkDirectiveMock} from "../../../../testing/router-link-mock.directive";
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule,
     DynamicComponentLoaderModule.forChild(SubstancePolymerStructureComponent)
   ],
-    declarations: [SubstancePolymerStructureComponent, RouterLinkDirectiveMock]
+    declarations: [SubstancePolymerStructureComponent]
 })
 export class SubstancePolymerStructureModule { }
