87f5bcce2ccc3081cb1752abb24093378ceee91b
Jorge Neyra <jorge.neyra@nih.gov>
2020-02-13 13:45:53 -0500

    created module and component

---
+++ src/app/core/substance-form/substance-form-change-reason/substance-form-change-reason.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-substance-form-change-reason',
+  templateUrl: './substance-form-change-reason.component.html',
+  styleUrls: ['./substance-form-change-reason.component.scss']
+})
+export class SubstanceFormChangeReasonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/core/substance-form/substance-form-change-reason/substance-form-change-reason.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SubstanceFormChangeReasonComponent } from './substance-form-change-reason.component';
+
+
+
+@NgModule({
+  declarations: [SubstanceFormChangeReasonComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SubstanceFormChangeReasonModule { }
