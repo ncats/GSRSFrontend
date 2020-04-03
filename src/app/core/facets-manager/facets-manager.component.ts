dd4665199fec20e9478f17c7c8e9dc1253ebb347
Jorge Neyra <jorge.neyra@nih.gov>
2020-04-03 16:06:53 -0400

    added facets files by mistake

---
+++ src/app/core/facets-manager/facets-manager.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-facets-manager',
+  templateUrl: './facets-manager.component.html',
+  styleUrls: ['./facets-manager.component.scss']
+})
+export class FacetsManagerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
