b2e900d19fbcd7298a09d58612b69f44bd3456a2
Jorge Neyra <jorge.neyra@nih.gov>
2020-05-13 10:10:35 -0400

    created components and module

---
+++ src/app/core/guided-search/guided-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-guided-search',
+  templateUrl: './guided-search.component.html',
+  styleUrls: ['./guided-search.component.scss']
+})
+export class GuidedSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/core/guided-search/guided-search.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GuidedSearchComponent } from './guided-search.component';
+
+
+
+@NgModule({
+  declarations: [GuidedSearchComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class GuidedSearchModule { }
