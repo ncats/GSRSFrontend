53d65cf1b689d5b8be0684196d5230e81acc230a
NikoAnderson <nikolaus.g.anderson@gmail.com>
2021-09-30 21:30:54 -0400

    edit name sort fix, clean structure stereochemistry fix

--- src/app/core/substance-form/names/substance-form-names-card.component.ts
+++ src/app/core/substance-form/names/substance-form-names-card.component.ts
@@ -52,6 +52,22 @@ export class SubstanceFormNamesCardComponent
       });
     this.subscriptions.push(definitionSubscription);
     const namesSubscription = this.substanceFormNamesService.substanceNames.subscribe(names => {
+      names.sort(function (a, b) {
+        if (a.displayName) {
+          return -1;
+        } else {
+          if (b.displayName) {
+            return 1;
+          }
+          if (a.name < b.name) {
+            return -1;
+          }
+          if (a.name > b.name) {
+            return 1;
+          }
+          return 0;
+        }
+      });
       this.names = names;
       this.filtered = names;
       const searchSubscription = this.searchControl.valueChanges.subscribe(value => {
