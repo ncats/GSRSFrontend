dc105849783ec79eeaf9f63aeaa9ed50aad1e15d
NikoAnderson <nikolaus.g.anderson@gmail.com>
2021-10-19 20:03:57 -0400

    unauthorized reroute fix

--- src/app/core/unauthorized/unauthorized.component.ts
+++ src/app/core/unauthorized/unauthorized.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ConfigService } from '@gsrs-core/config';
+import { AuthService } from '@gsrs-core/auth';
+import { take } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-unauthorized',
@@ -9,13 +12,21 @@ import { ConfigService } from '@gsrs-core/config';
 export class UnauthorizedComponent implements OnInit {
   email?: string;
   constructor(
-    private configService: ConfigService
+    private configService: ConfigService,
+    private authService: AuthService,
+    private router: Router
   ) { }
 
   ngOnInit() {
     if (this.configService.configData && this.configService.configData.contactEmail) {
       this.email = this.configService.configData.contactEmail;
     }
+
+    this.authService.getAuth().pipe(take(1)).subscribe(response => {
+      if (response && response.active) {
+        this.router.navigate(['/home']);
+      }
+    });
   }
 
 }
