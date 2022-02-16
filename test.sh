fa90acadf9bc41360d4b0e1e8eed5826c1d22e22
NikoAnderson <nikolaus.g.anderson@gmail.com>
2022-02-16 13:52:23 -0500

    Create test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 setup_git() {
-  git clone --depth=50 https://${GIT_ACCESS_TOKEN}@github.com/ncats/gsrs-ci ncats/gsrs-ci
+  git clone --depth=50 https://github.com/ncats/gsrs-ci ncats/gsrs-ci
   ls -l
 }
 
