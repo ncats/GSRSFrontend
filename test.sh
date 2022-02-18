de157232268216f067c0df5532a6b60971e29dd0
NikoAnderson <nikolaus.g.anderson@gmail.com>
2022-02-18 18:01:33 -0500

    Create test.sh

--- test.sh
+++ test.sh
@@ -4,14 +4,15 @@ setup_git() {
   git clone --depth=50 -b fda https://GsrsBot:${GIT_ACCESS_TOKEN}@github.com/ncats/gsrs-ci.git
   ls -l
   git stash
+  git fetch
   cd gsrs-ci
   git status
-  git checkout -b fda_travis_sync
+  git checkout fda_travis_sync
   git merge fda
 cp -r frontend/src/main/resources/static/substanceRelationshipVisualizer ./
 rm -rf frontend/src/main/resources/static
 mkdir frontend/src/main/resources/static
-cp -r ../workspace/GSRSFrontend/dist/browser/* frontend/src/main/resources/static/
+cp -r ../GSRSFrontend/dist/browser/* frontend/src/main/resources/static/
 git add frontend/src/main/resources/static
 git add -u 
 git commit -m "travis test before build"
