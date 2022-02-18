4598833800568123bd2758beb934d48c82c275e6
NikoAnderson <nikolaus.g.anderson@gmail.com>
2022-02-18 16:16:04 -0500

    Create test.sh

--- test.sh
+++ test.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
 setup_git() {
-  git clone --depth=50 https://GsrsBot:${GIT_ACCESS_TOKEN}@github.com/ncats/gsrs-ci.git
+  git clone --depth=50 -b fda https://GsrsBot:${GIT_ACCESS_TOKEN}@github.com/ncats/gsrs-ci.git
   ls -l
   git stash
   cd gsrs-ci
-git checkout fda
-git pull origin fda
-git checkout fda_travis_sync
-git merge fda
+  git status
 cp -r frontend/src/main/resources/static/substanceRelationshipVisualizer ./
 rm -rf frontend/src/main/resources/static
 mkdir frontend/src/main/resources/static
