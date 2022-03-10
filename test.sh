39c90d9d846022a790dddacd218c83cbce9102a5
NikoAnderson <nikolaus.g.anderson@gmail.com>
2022-03-10 17:11:49 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 setup_git() {
   cd ../
   git clone --depth=50 -b fda_travis_sync https://GsrsBot:${GIT_ACCESS_TOKEN}@github.com/ncats/gsrs-ci.git
-
+git init
 cp -r frontend/src/main/resources/static/substanceRelationshipVisualizer ./
 rm -rf frontend/src/main/resources/static
 mkdir frontend/src/main/resources/static
@@ -11,8 +11,10 @@ cp -r ../GSRSFrontend/dist/browser/* frontend/src/main/resources/static/
 git add frontend/src/main/resources/static
 git add -u 
 git commit -m "travis test before build"
-git push -u origin fda_travis_sync
+git status
 ls
+git push -u origin fda_travis_sync
+
 
 
 }
