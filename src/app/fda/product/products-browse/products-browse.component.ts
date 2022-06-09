eb3d6440f9679a1f1f721a0f187c7c70a8333c16
Newatia <Archana.Newatia@fda.gov>
2022-06-09 16:14:30 -0400

    tickets for 3.0.2

--- src/app/fda/product/products-browse/products-browse.component.ts
+++ src/app/fda/product/products-browse/products-browse.component.ts
@@ -66,6 +66,8 @@ export class ProductsBrowseComponent implements OnInit, AfterViewInit, OnDestroy
   isSearchEditable = false;
   lastPage: number;
   invalidPage = false;
+  iconSrcPath = '';
+  dailyMedUrl = '';
   ascDescDir = 'desc';
   public displayedColumns: string[] = [
     'productNDC',
@@ -152,6 +154,9 @@ export class ProductsBrowseComponent implements OnInit, AfterViewInit, OnDestroy
       this.isAdmin = this.authService.hasAnyRoles('Admin', 'Updater', 'SuperUpdater');
     });
     this.subscriptions.push(authSubscription);
+
+    this.iconSrcPath = `${this.configService.environment.baseHref || ''}assets/icons/fda/icon_dailymed.png`;
+    this.dailyMedUrl = 'https://dailymed.nlm.nih.gov/dailymed/search.cfm?labeltype=all&query=';
 
     this.isComponentInit = true;
     this.loadComponent();
