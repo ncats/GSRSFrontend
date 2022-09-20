2e75022adeb3112b6363684448bcf26521588c7f
Newatia <Archana.Newatia@fda.gov>
2022-09-20 10:11:13 -0400

    added configurable header authentication

--- src/app/core/auth/auth.interceptor.ts
+++ src/app/core/auth/auth.interceptor.ts
@@ -3,26 +3,55 @@ import {
     HttpEvent, HttpInterceptor, HttpHandler, HttpRequest
 } from '@angular/common/http';
 import { isPlatformBrowser } from '@angular/common';
-
 import { Observable } from 'rxjs';
+import { ConfigService } from '@gsrs-core/config/config.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-    constructor(@Inject(PLATFORM_ID) private platformId: any) {
+    constructor(
+        @Inject(PLATFORM_ID) private platformId: any,
+        public configService: ConfigService) {
     }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
         if (isPlatformBrowser(this.platformId)) {
+
+            // Reading from config file, and adding additional header
+            if (this.configService.configData) {
+                if (this.configService.configData.authenticateAs.apiUsername !== null) {
+                    req = req.clone({
+                        headers: req.headers.set('auth-username', this.configService.configData.authenticateAs.apiUsername)
+                    });
+                }
+                if (this.configService.configData.authenticateAs.apiPassword !== null) {
+                    req = req.clone({
+                        headers: req.headers.set('auth-password', this.configService.configData.authenticateAs.apiPassword)
+                    });
+                }
+
+                if (this.configService.configData.authenticateAs.apiKey !== null) {
+                    req = req.clone({
+                        headers: req.headers.set('auth-key', this.configService.configData.authenticateAs.apiKey)
+                    });
+                }
+
+                if (this.configService.configData.authenticateAs.apiToken !== null) {
+                    req = req.clone({
+                        headers: req.headers.set('auth-token', this.configService.configData.authenticateAs.apiToken)
+                    });
+                }
+
+            } // configData end
+
             const authToken = sessionStorage.getItem('authToken');
             if (authToken) {
                 req = req.clone({
                     headers: req.headers.set('auth-token', authToken)
                 });
             }
-        }
 
-        return next.handle(req);
+            return next.handle(req);
+        }
     }
 }
