08f2265066efacce9c9c8e3dbb297867614ab134
Newatia <Archana.Newatia@fda.gov>
2022-09-23 16:45:25 -0400

    added this in function

--- src/app/fda/service/sso-refresh.service.ts
+++ src/app/fda/service/sso-refresh.service.ts
@@ -18,48 +18,48 @@ export class SsoRefreshService implements OnDestroy {
     private configService: ConfigService
   ) {
     if (isPlatformBrowser(this.platformId)) {
-     
+
       if (window.location.pathname.indexOf('/ginas/app/beta/') > -1) {
         this.baseHref = '/ginas/app/beta/';
       }
     }
-}
+  }
 
-updateIframe(): any {
-      if(!this.iframe){
-        this.iframe = document.createElement('IFRAME') as HTMLIFrameElement;
-        this.iframe.title = 'page refresher';
-        this.iframe.name = 'refresher';
-        this.iframe.style.height = '0';
-        this.iframe.style.opacity = '0';
-        this.iframe.src = `${this.baseHref || ''}assets/refresh/refresh.html?key=${this.utilsService.newUUID()}`;
-        document.body.appendChild(this.iframe);
-      }else{
-        this.iframe.src = `${this.baseHref || ''}assets/refresh/refresh.html?key=${this.utilsService.newUUID()}`;
-      }
-}
-
-init(): any {
-  this.authService.getAuth().subscribe(auth => {
-    if (auth != null && this.refreshInterval == null) {
-      const homeBaseUrl = this.configService.configData && this.configService.configData.gsrsHomeBaseUrl || null;
-      if (homeBaseUrl) {
-        this.baseHref = homeBaseUrl;
-        updateIframe();
-      }
-      clearInterval(this.refreshInterval);
-      this.refreshInterval = setInterval(() => {
-        updateIframe();
-      }, 120000);
+  updateIframe(): any {
+    if (!this.iframe) {
+      this.iframe = document.createElement('IFRAME') as HTMLIFrameElement;
+      this.iframe.title = 'page refresher';
+      this.iframe.name = 'refresher';
+      this.iframe.style.height = '0';
+      this.iframe.style.opacity = '0';
+      this.iframe.src = `${this.baseHref || ''}assets/refresh/refresh.html?key=${this.utilsService.newUUID()}`;
+      document.body.appendChild(this.iframe);
     } else {
-      clearInterval(this.refreshInterval);
-      this.refreshInterval = null;
+      this.iframe.src = `${this.baseHref || ''}assets/refresh/refresh.html?key=${this.utilsService.newUUID()}`;
     }
-  });
-}
+  }
 
-ngOnDestroy() {
-  clearInterval(this.refreshInterval);
-  this.refreshInterval = null;
-}
+  init(): any {
+    this.authService.getAuth().subscribe(auth => {
+      if (auth != null && this.refreshInterval == null) {
+        const homeBaseUrl = this.configService.configData && this.configService.configData.gsrsHomeBaseUrl || null;
+        if (homeBaseUrl) {
+          this.baseHref = homeBaseUrl;
+          this.updateIframe();
+        }
+        clearInterval(this.refreshInterval);
+        this.refreshInterval = setInterval(() => {
+          this.updateIframe();
+        }, 120000);
+      } else {
+        clearInterval(this.refreshInterval);
+        this.refreshInterval = null;
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.refreshInterval);
+    this.refreshInterval = null;
+  }
 }
