a24500809f46d559e7acc811a790e938faef5191
A. Welsch <alexander.welsch@fda.hhs.gov>
2022-10-14 16:22:12 -0400

    minor edits

--- src/app/core/app.module.ts
+++ src/app/core/app.module.ts
@@ -77,7 +77,6 @@ import { GuidedSearchModule } from './guided-search/guided-search.module';
 import { CanActivateAdminPage } from '@gsrs-core/admin/can-activate-admin-page';
 import { ExportDialogComponent } from '@gsrs-core/substances-browse/export-dialog/export-dialog.component';
 import { TextInputFormComponent } from '@gsrs-core/utils/text-input-form/text-input-form.component';
-
 import { NamesDisplayPipe } from '@gsrs-core/utils/names-display-order.pipe';
 // eslint-disable-next-line max-len
 import { BrowseHeaderDynamicSectionDirective } from '@gsrs-core/substances-browse/browse-header-dynamic-section/browse-header-dynamic-section.directive';
