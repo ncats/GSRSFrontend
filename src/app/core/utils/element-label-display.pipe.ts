3ca0c6f7ec8d70a457da3785a070a7d16c8684b7
A. Welsch <alexander.welsch@fda.hhs.gov>
2022-10-20 16:01:09 -0400

    add default config for element display label

--- src/app/core/utils/element-label-display.pipe.ts
+++ src/app/core/utils/element-label-display.pipe.ts
@@ -5,28 +5,45 @@ import { ConfigService } from '@gsrs-core/config';
   name: 'elementLabel'
 })
 export class ElementLabelDisplayPipe implements PipeTransform {
-  
+
   private labels: any; 
+  private defaultLabels: any;
   private conf: any; 
+  // labels.group.key.value
+  private defaultConf =
+  {"elementLabelDisplay": {
+      "labels": {
+        "substance_names_name": {
+            "displayNameTitle": "Display Name",
+            "displayNameShortTitle":"DN",
+            "preferredTitle": "Additional Listing Name",
+            "preferredShortTitle": "AL"
+        }
+      }
+    }
+  };
+
   private confOK: boolean = false;
   constructor(
     configService: ConfigService
   ) { 
       this.conf = configService.configData.elementLabelDisplay;
       this.labels = configService.configData.elementLabelDisplay?.labels;
+      this.defaultLabels = this.defaultConf.elementLabelDisplay.labels;
       if(this.conf && this.labels) {
         this.confOK = true;     
-      } else {
-        console.log("Warning, No configuration data for elementLabelDisplay.labels");
-      }
+      }  
   }
   transform(key?: string, group?: string): string {
     if(this.confOK && this.labels[group] && this.labels[group][key]) {
       return this.labels[group][key];
+    } else if(this.defaultLabels && this.defaultLabels[group] && this.defaultLabels[group][key]) { 
+      return this.defaultLabels[group][key];
     } else if(key) {
       return key;
     } else {
       return "UNKOWN ELEMENT LABEL KEY/VALUE";
     }	
   }
-}  
+}
+
