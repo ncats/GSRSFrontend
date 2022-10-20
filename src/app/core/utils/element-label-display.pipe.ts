5b420c48754a5c4afb2df00f03ef903c4c9a6886
A. Welsch <alexander.welsch@fda.hhs.gov>
2022-10-19 22:07:30 -0400

    element label debug

--- src/app/core/utils/element-label-display.pipe.ts
+++ src/app/core/utils/element-label-display.pipe.ts
@@ -7,21 +7,26 @@ import { ConfigService } from '@gsrs-core/config';
 export class ElementLabelDisplayPipe implements PipeTransform {
   
   private labels: any; 
-
+  private conf: any; 
+  private confOK: boolean = false;
   constructor(
     configService: ConfigService
   ) { 
-      this.labels = configService.configData.elementLabelDisplay.labels;
-  }
-
-  transform(key: string, group: string): string {
-
-    if(this.labels[group] && this.labels[group][key]) {
-        return this.labels[group][key] + " PIPED";
-      } else if(key) {
-        return key;
+      this.conf = configService.configData.elementLabelDisplay;
+      this.labels = configService.configData.elementLabelDisplay?.labels;
+      if(this.conf && this.labels) {
+        this.confOK = true;     
       } else {
-        return "UNKNOWN ELEMENT LABEL";
-      }	
-  }  
-}
+        console.log("Warning, No configuration data for elementLabelDisplay.labels");
+      }
+  }
+  transform(key?: string, group?: string): string {
+    if(this.confOK && this.labels[group] && this.labels[group][key]) {
+      return this.labels[group][key];
+    } else if(key) {
+      return key;
+    } else {
+      return "UNKOWN ELEMENT LABEL KEY/VALUE";
+    }	
+  }
+}  
