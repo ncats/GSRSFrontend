c0d886a457fa53688e81dc707a1449e2ba45f9fd
NikoAnderson <nikolaus.g.anderson@gmail.com>
2023-03-01 20:52:23 -0500

    travis branch

--- travisScript.sh
+++ travisScript.sh
@@ -10,7 +10,7 @@ git status
 git fetch
 git branch -a
 git status
-git checkout fda_staged_sync
+git checkout import_frontend
   git pull
   git merge origin/fda
   cd gsrs-ci
@@ -28,38 +28,10 @@ git commit -m "frontend commit ${TRAVIS_COMMIT} build of branch ${TRAVIS_BRANCH}
 git status
 ls
 echo "pushing"
-git push -u origin fda_staged_sync
+git push -u origin import_frontend
 
 
- cd ../
-  rm -rf gsrs-ci
-  ls -l
-  git clone https://GsrsBot:${GIT_ACCESS_TOKEN}@github.com/ncats/gsrs-ci.git
-cd gsrs-ci
-git status
-git fetch
-git branch -a
-git status
-git checkout master_staged_sync
-git pull
-git merge origin/master
-cp -r frontend/src/main/resources/static/substanceRelationshipVisualizer ./
-cp frontend/src/main/resources/static/assets/data/config.json ./
-rm -rf frontend/src/main/resources/static
-mkdir frontend/src/main/resources/static
 
-cp -r ../GSRSFrontend/dist/browser/* frontend/src/main/resources/static/
-cp -r ./substanceRelationshipVisualizer frontend/src/main/resources/static/
-cp ./config.json frontend/src/main/resources/static/assets/data/
-rm ./config.json
-rm -rf ./substanceRelationshipVisualizer
-git add frontend/src/main/resources/static
-git add -u 
-git commit -m "frontend commit ${TRAVIS_COMMIT} build of branch ${TRAVIS_BRANCH} "
-git status
-ls
-git push -u origin master_staged_sync
-}
 
 
 setup_git
