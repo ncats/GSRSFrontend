e8b0bbd10746078b923945661b9e5af3b6ffbe69
alx652 <welscha2@nih.gov>
2024-06-18 13:56:02 -0400

    make it so beta redirects to ui

--- src/app/core/page-not-found/page-not-found.component.ts
+++ src/app/core/page-not-found/page-not-found.component.ts
@@ -13,7 +13,21 @@ export class PageNotFoundComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.router.navigate(['/home']);
+    let url=window.location.href;
+    console.log("url: " + url);
+    const regex1 = /\/ginas\/app\/beta\//;
+    const regex2 = /\ginas\/app\/ui\//;
+    if(url.match(regex1)) { 
+      url = url.replace(regex1, "/ginas/app/ui/");
+      const parts = url.split(regex2);
+      if (parts.length > 2) {
+        const replaced = parts.slice(0, -1).join('');
+        url =  replaced + 'ginas/app/ui/' + parts[parts.length - 1];
+        window.location.href = url;
+      }
+    } else { 
+      this.router.navigate(['/home']);
+    } 
   }
 
 }
