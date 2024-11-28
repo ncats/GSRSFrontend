625b772518d1109ef9bc57eac36e1e9f44928a3c
Petr Barta <pbarta@dnanexus.com>
2024-11-28 15:11:28 +0100

    Public GSRS

--- src/app/core/base/base-http.service.ts
+++ src/app/core/base/base-http.service.ts
@@ -2,16 +2,12 @@ import { ConfigService } from '../config/config.service';
 
 export abstract class BaseHttpService {
   public apiBaseUrl: string;
-  public pfdaApiBaseUrl: string = '';
   public baseUrl: string;
 
   constructor(
     public configService: ConfigService
   ) {
     this.apiBaseUrl = `${(this.configService.configData && this.configService.configData.apiBaseUrl) || '/' }api/v1/`;
-    if (this.configService.configData.isPfdaVersion && this.configService.configData.pfdaApiBaseUrl) {
-      this.pfdaApiBaseUrl = this.configService.configData.pfdaApiBaseUrl;
-    }
     this.baseUrl = (this.configService.configData && this.configService.configData.apiBaseUrl) || '/';
   }
 }
