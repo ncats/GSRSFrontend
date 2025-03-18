3df1b42b08871b4c2339cb4213aaa2b6336c1bfc
NikoAnderson <nikolaus.g.anderson@gmail.com>
2025-03-18 12:30:51 -0400

    deleting root files for external libraries

--- projects/file-select/src/public_api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of file-select
- */
-
-export * from '../../../src/app/core/file-select/file-select.directive';
-export * from '../../../src/app/core/file-select/file-select.module';
--- projects/ketcher-wrapper/src/public_api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of ketcher-wrapper
- */
-
-export * from './lib/ketcher-wrapper.service';
-export * from './lib/ketcher-wrapper.component';
-export * from './lib/ketcher-wrapper.module';
-export * from './lib/ketcher.model';
