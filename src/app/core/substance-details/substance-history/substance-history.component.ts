d5f0d0442e2bb18734a2504896accf844f133dc7
NikoAnderson <nikolaus.g.anderson@gmail.com>
2025-03-21 10:04:19 -0400

    GSK changes

--- src/app/core/substance-details/substance-history/substance-history.component.ts
+++ src/app/core/substance-details/substance-history/substance-history.component.ts
@@ -173,7 +173,7 @@ getOldValue(diff: SubstanceDiff) {
     path = path.replace(/\.(\d+)(?=[^0-9])/g, '[$1]')
     result = jsonpath.query(this.substanceOldValue, '$' + path);
     if (result.length > 0) {
-      result = this.getDisplayValue(diff, result[0]);
+      result = this.getDisplayValue(diff, result[0],'old');
     }
   } else {
     result = 'Not Applicable'
