f023b933079945efa5e1f4d2bb513d5e6ae11512
Jaroslav Iha <jiha@dnanexus.com>
2026-02-24 12:31:26 +0100

    show no image for no structure

--- src/app/core/substance/substance-image.directive.ts
+++ src/app/core/substance/substance-image.directive.ts
@@ -1,10 +1,10 @@
-import { Directive, ElementRef, Input, AfterViewInit } from '@angular/core';
-import { UtilsService } from '../utils/utils.service';
-import { HttpClient } from '@angular/common/http';
-import { ConfigService } from '@gsrs-core/config/config.service';
+import { Directive, ElementRef, Input, AfterViewInit } from "@angular/core";
+import { UtilsService } from "../utils/utils.service";
+import { HttpClient } from "@angular/common/http";
+import { ConfigService } from "@gsrs-core/config/config.service";
 
 @Directive({
-  selector: '[appSubstanceImage]'
+  selector: "[appSubstanceImage]",
 })
 export class SubstanceImageDirective implements AfterViewInit {
   private privateEntityId: string;
@@ -20,13 +20,16 @@ export class SubstanceImageDirective implements AfterViewInit {
     private el: ElementRef,
     private utilsService: UtilsService,
     private configService: ConfigService,
-    private http: HttpClient
+    private http: HttpClient,
   ) {
     this.imageElement = this.el.nativeElement as HTMLImageElement;
   }
 
   ngAfterViewInit() {
     this.isAfterViewInit = true;
+    this.imageElement.onerror = () => {
+      this.setNoImage();
+    };
     this.setImageSrc();
   }
 
@@ -79,12 +82,20 @@ export class SubstanceImageDirective implements AfterViewInit {
   }
 
   private setImageSrc(): void {
-    const useDataUrlConfig = this.configService.configData && this.configService.configData.useDataUrl || false;
+    const useDataUrlConfig =
+      (this.configService.configData &&
+        this.configService.configData.useDataUrl) ||
+      false;
     if (this.isAfterViewInit) {
       if (this.privateEntityId) {
         if (this.privateVersion) {
           const srcUrl = this.utilsService.getStructureImgUrl(
-            this.privateEntityId, this.privateSize, this.privateStereo, this.privateAtomMaps, this.privateVersion);
+            this.privateEntityId,
+            this.privateSize,
+            this.privateStereo,
+            this.privateAtomMaps,
+            this.privateVersion,
+          );
           if (useDataUrlConfig === true) {
             this.setImageSrcAsBlob(srcUrl);
           } else {
@@ -92,55 +103,85 @@ export class SubstanceImageDirective implements AfterViewInit {
           }
         } else {
           let srcUrl = this.utilsService.getStructureImgUrl(
-            this.privateEntityId, this.privateSize, this.privateStereo, this.privateAtomMaps);
+            this.privateEntityId,
+            this.privateSize,
+            this.privateStereo,
+            this.privateAtomMaps,
+          );
           if (this.privateAltId) {
             srcUrl = this.utilsService.getStructureImgUrl(
-              this.privateEntityId, this.privateSize, this.privateStereo, this.privateAtomMaps, null, this.privateAltId);
+              this.privateEntityId,
+              this.privateSize,
+              this.privateStereo,
+              this.privateAtomMaps,
+              null,
+              this.privateAltId,
+            );
           } else {
           }
-         
+
           if (useDataUrlConfig === true) {
             this.setImageSrcAsBlob(srcUrl);
           } else {
             this.imageElement.src = srcUrl;
           }
         }
-    } else { 
-      let srcUrl =`${this.configService.environment.baseHref || ''}assets/images/noimage.svg`;
-
-      if(this.privateAltId) {
-        srcUrl = this.utilsService.getStructureImgUrl(
-          this.privateEntityId, this.privateSize, this.privateStereo, this.privateAtomMaps, null, this.privateAltId);
-      }
-      if(this.privateSize){
-        this.imageElement.height = this.privateSize;
-        this.imageElement.width = this.privateSize;
-        }else{
-              this.imageElement.height = 150;
-              this.imageElement.width = 150;
-        }
-      if (useDataUrlConfig === true) {
-        this.setImageSrcAsBlob(srcUrl);
       } else {
-        this.imageElement.src = srcUrl;
+        if (this.privateAltId) {
+          const srcUrl = this.utilsService.getStructureImgUrl(
+            this.privateEntityId,
+            this.privateSize,
+            this.privateStereo,
+            this.privateAtomMaps,
+            null,
+            this.privateAltId,
+          );
+          if (useDataUrlConfig === true) {
+            this.setImageSrcAsBlob(srcUrl);
+          } else {
+            this.imageElement.src = srcUrl;
+          }
+        } else {
+          this.setNoImage();
+        }
       }
-
+      this.imageElement.alt = "structure image";
     }
-      this.imageElement.alt = 'structure image';
+  }
+
+  private setNoImage(): void {
+    const noImageUrl = `${this.configService.environment.baseHref || ""}assets/images/noimage.svg`;
+    // Temporarily remove onerror to avoid infinite loop if noimage.svg itself fails
+    this.imageElement.onerror = null;
+    this.imageElement.src = noImageUrl;
+    if (this.privateSize) {
+      this.imageElement.height = this.privateSize;
+      this.imageElement.width = this.privateSize;
+    } else {
+      this.imageElement.height = 150;
+      this.imageElement.width = 150;
     }
   }
 
   private setImageSrcAsBlob(srcUrl: any): void {
     // Getting Image Source as Blob
-    this.http.get(srcUrl, { responseType: "blob" }).subscribe(imgDat => {
-      let reader = new FileReader();
-      reader.addEventListener("load", () => {
-        this.imageElement.src = reader.result.toString();
-      }, false);
-      if (imgDat) {
-        reader.readAsDataURL(imgDat);
-      }
-    });
+    this.http.get(srcUrl, { responseType: "blob" }).subscribe(
+      (imgDat) => {
+        let reader = new FileReader();
+        reader.addEventListener(
+          "load",
+          () => {
+            this.imageElement.src = reader.result.toString();
+          },
+          false,
+        );
+        if (imgDat) {
+          reader.readAsDataURL(imgDat);
+        }
+      },
+      () => {
+        this.setNoImage();
+      },
+    );
   }
-
 }
