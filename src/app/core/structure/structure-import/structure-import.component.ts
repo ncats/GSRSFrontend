d77280c385f7cf9e51fecbea287b9176ec10fc12
Iaroslav Moskviak <moskviaki2@nih.gov>
2026-05-22 13:59:51 -0400

    v3000 error fix

--- src/app/core/structure/structure-import/structure-import.component.ts
+++ src/app/core/structure/structure-import/structure-import.component.ts
@@ -32,7 +32,7 @@ export class StructureImportComponent implements OnInit {
       this.isLoading = true;
       this.structureService.interpretStructure(this.importTextControl.value).subscribe(response => {
         this.isLoading = false;
-        if (response && response.structure && response.structure.molfile) {
+        if (response && response.structure && response.structure.molfile && response.structure.smiles) {
           this.gaService.sendEvent('structureImport', 'button:import', 'file imported');
           this.dialogRef.close(response);
         } else {
